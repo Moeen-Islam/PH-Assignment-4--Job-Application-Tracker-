4a395b6a7ff40d72cf2d0a3c90936ad176fe9b08
Moeen-Islam <moeenislam809@gmail.com>
2026-02-23 21:34:46 +0600

    Render Function added and implement

--- script.js
+++ script.js
@@ -153,7 +153,79 @@ let jobs = [
     renderJobs();
   }
 
+  function renderJobs() {
+    const jobList = document.getElementById("jobList");
+    const emptyState = document.getElementById("emptyState");
 
+    jobList.innerHTML = "";
+
+    let filteredJobs;
+
+    if (currentFilter === "all") {
+        filteredJobs = jobs; 
+    } else {
+        filteredJobs = jobs.filter(job => job.status === currentFilter);
+        }
+
+    if(filteredJobs.length === 0){
+        emptyState.classList.remove("hidden");
+    }else {
+        emptyState.classList.add("hidden");
+        }
+
+    filteredJobs.forEach(job => {
+      jobList.innerHTML += `
+        <div class="bg-white p-5 rounded-lg shadow flex flex-col sm:flex-row gap-4 sm:justify-between ">
+          
+          <div class="space-y-6 w-full">
+
+          <div class="flex justify-between items-center">
+          <div class ="space-y-1.5">
+            <h3 class="text-lg font-bold text-[#002C5C]">${job.company}</h3>
+            <p class="text-sm text-[#64748B]">${job.role}</p>
+          </div>
+            
+            <button onclick="deleteJob(${job.id})" type="button" aria-label="Delete job" class="cursor-pointer p-1 rounded bg-transparent border-none hover:bg-red-50 transition">
+                 <i class="fa-solid fa-trash-can text-[#002C5C] hover:text-red-600 transition-colors"></i>
+            </button>
+          </div>
+            
+           <p class="text-sm text-[#64748B] mt-2">
+                <span>${job.location}</span>
+                <span class="mx-2">•</span>
+                <span>${job.jobType}</span>
+                <span class="mx-2">•</span>
+                <span>${job.salary}</span>
+            </p>
+
+            <span class="inline-block mt-2 px-3 py-1 text-xs text-[#002C5C] font-semibold rounded ${getStatus(job.status)}">
+              ${job.status.replace("_", " ").toUpperCase()}
+            </span>
+
+            <p class="text-sm text-[#323B49] mt-2">
+              ${job.description}
+            </p>
+
+            <div class="mt-4 flex flex-wrap gap-2">
+              <button onclick="updateStatus(${job.id}, 'interview')"
+                class="px-3 py-1 border text-[14px] border-green-500 rounded-md mr-2 cursor-pointer text-green-500 font-semibold">
+                INTERVIEW
+              </button>
+
+              <button onclick="updateStatus(${job.id}, 'rejected')"
+                class="px-3 py-1 border text-[14px] border-red-500 rounded-md cursor-pointer text-red-500 font-semibold">
+                REJECTED
+              </button>
+            </div>
+          </div>
+        </div>
+      `;
+    });
+
+    updateCounts(filteredJobs);
+  }
+
+  renderJobs();
 
 
 
